85da5d68f49e0aee7b82d15d3d48321f112e700b
prevolley <prevolley@gmail.com>
2016-06-10 17:47:57 +0300

    update(main.js):fix control

--- js/main.js
+++ js/main.js
@@ -2,129 +2,128 @@ var ractive = new Ractive({
 	el: 'ract',
 	template: '#template',
 	data: {
-	planeX: 100,
-	planeY: 0,
-	skin: "img/airplane2.png"
+	planeX: 270,
+	planeY: 400,
+	skin: "img/airplane2.png",
+	CANVAS_WIDTH: 600,
+	CANVAS_HEIGHT: 500,
+	PLANE_WIDTH: 60,
+	PLANE_HEIGHT: 47
 	}
 }),
-	CANVAS_WIDTH = 600;
-	CANVAS_HEIGHT = 500;
-	PLANE_WIDTH = 50;
-	PLANE_HEIGHT = 50;
+	CANVAS_WIDTH = ractive.get( 'CANVAS_WIDTH' );
+	CANVAS_HEIGHT = ractive.get( 'CANVAS_HEIGHT' );
 	SPEED = 8,
 	LEFT=37, UP=38, RIGHT=39, DOWN=40,
 	dirs = {LEFT:0, UP:0, RIGHT:0, DOWN:0},
-	FRAMES_PER_SECOND = 60;
-
-/*var player = function() {
-	this._x = 0;
-	this._y = 0;
-	this.x: {
-		get: function() {
-			return this._x;
-		}
-		set: function(num) {
-			this._x = num;
-		}
-	}
-};*
-
-var circle = {
-    radius: 2,
-    get diameter() {
-        return this.radius * 2;
-    },
-    set diameter(val) {
-        this.radius = val / 2;
-    }
-}*/
+	FRAMES_PER_SECOND = 60,
+	layout = document.getElementById('layout');
 
 var player = {
-	get position() {
-		return {
-			x: ractive.get( 'planeX' ),
-			y: ractive.get( 'planeY' )
+	position: {
+		get x() {
+			return ractive.get( 'planeX' )
+		},
+		get y() {
+			return ractive.get( 'planeY' )
+		},
+		set x(val) {
+			ractive.set({'planeX': val})
+		},
+		set y(val) {
+			ractive.set({'planeY': val})
 		}
-	},
-	set position(pos) {
-		ractive.set({'planeX': pos.x});
-		ractive.set({'planeY': pos.y});
 	},
 	direction: {
 		x: 0,
 		y: 0
-	}
+	},
+	width: ractive.get( 'PLANE_WIDTH' ),
+	height: ractive.get( 'PLANE_HEIGHT' ),
+	speed: 2
 }
-// player.position = {x:0,y:100}
+
+function createEnemy(){
+	var svgns = "http://www.w3.org/2000/svg";
+	var el = document.createElementNS(svgns, 'image');
+	el.setAttribute('x', 200);
+	el.setAttribute('y', 200);
+	el.setAttribute('width', 60);
+	el.setAttribute('height', 47);
+	el.setAttribute('href', 'img/enemy.png');
+
+	var svg = document.getElementsByTagNameNS(svgns, 'svg')[0];
+	svg.appendChild(el);
+}
 
 function handleInput() {
 	window.addEventListener("keydown", function(event) {
 	if (event.keyCode == LEFT)
 	{
+		player.direction.x = -1;
 		dirs.LEFT = 1;
+		ractive.set({'skin': "img/airplane2left.png"});
 		console.log('left ' + dirs.LEFT);
 	}
 	if (event.keyCode == RIGHT){
+		player.direction.x = 1;
 		dirs.RIGHT = 1;
+		ractive.set({'skin': "img/airplane2right.png"});
 		console.log('right ' + dirs.RIGHT);
 	}
 	if (event.keyCode == UP){
+		player.direction.y = -1;
 		dirs.UP = 1;
 		console.log('up ' + dirs.UP);
 	}
 	if (event.keyCode == DOWN){
+		player.direction.y = 1;
 		dirs.DOWN = 1;
 		console.log('down ' + dirs.DOWN);
 	}
 	});
 	window.addEventListener("keyup", function(event) {
-	if (event.keyCode == LEFT)
+	if (event.keyCode == LEFT && player.direction.x == -1)
 	{
+		player.direction.x = 0;
 		dirs.LEFT = 0;
+		ractive.set({'skin': "img/airplane2.png"});
 		console.log('left ' + dirs.LEFT);
 	}
-	if (event.keyCode == RIGHT){
+	if (event.keyCode == RIGHT && player.direction.x == 1){
+		player.direction.x = 0;
 		dirs.RIGHT = 0;
+		ractive.set({'skin': "img/airplane2.png"});
 		console.log('right ' + dirs.RIGHT);
 	}
-	if (event.keyCode == UP){
+	if (event.keyCode == UP && player.direction.y == -1){
+		player.direction.y = 0;
 		dirs.UP = 0;
 		console.log('up ' + dirs.UP);
 	}
-	if (event.keyCode == DOWN){
+	if (event.keyCode == DOWN && player.direction.y == 1){
+		player.direction.y = 0;
 		dirs.DOWN = 0;
 		console.log('down ' + dirs.DOWN);
 	}
 	});
-	/*
-	window.addEventListener("keydown", function(event) {
-	if (event.keyCode == 65)
-		ractive.set({'planeX': ractive.get( 'planeX' ) - 1});
-	if (event.keyCode == 68)
-		ractive.set({'planeX': ractive.get( 'planeX' ) + 1});
-	if (event.keyCode == 87)
-		ractive.set({'planeY': ractive.get( 'planeY' ) - 1});
-	if (event.keyCode == 83)
-		ractive.set({'planeY': ractive.get( 'planeY' ) + 1});
-	});
-	}*/
- 
 }
 handleInput();
 
 function step() {
-  requestAnimationFrame(step);
-  	if(ractive.get( 'planeX' ) > 0){
-		ractive.set({'planeX': ractive.get( 'planeX' ) - dirs.LEFT * SPEED});
-	}
-	if(ractive.get( 'planeX' ) < CANVAS_WIDTH - PLANE_WIDTH){
-		ractive.set({'planeX': ractive.get( 'planeX' ) + dirs.RIGHT * SPEED});
-	}
-	if(ractive.get( 'planeY' ) > 0){
-		ractive.set({'planeY': ractive.get( 'planeY' ) - dirs.UP * SPEED});
-	}
-	if(ractive.get( 'planeY' ) < CANVAS_HEIGHT - PLANE_HEIGHT){
-		ractive.set({'planeY': ractive.get( 'planeY' ) + dirs.DOWN * SPEED});
-	}
+	requestAnimationFrame(step);
+		if(player.direction.x != 0 && player.direction.y != 0) {
+			player.position.x += (player.direction.x * player.speed)*(3/5);
+			player.position.y += (player.direction.y * player.speed)*(3/5);
+		}
+		else{
+			player.position.x += player.direction.x * player.speed;
+			player.position.y += player.direction.y * player.speed;
+		}
+		if(player.position.x < 0) player.position.x = 0;
+		if(player.position.y < 0) player.position.y = 0;
+		if(player.position.x > CANVAS_WIDTH - player.width ) player.position.x = CANVAS_WIDTH - player.width;
+		if(player.position.y > CANVAS_HEIGHT - player.height ) player.position.y = CANVAS_HEIGHT - player.height;
+		console.log(player.position.x,player.position.y);
 }
 step();
